79d58d46bb629063fa4c64b1b622c06b358f0d99
Locco99 <collinsodenyo58@gmail.com>
2025-03-13 20:14:26 +0300

    First day assignment for python

---
+++ Assignment.py
@@ -0,0 +1,21 @@
+num1 = 10 # Integer Data Type
+num2 = 15 # Integer Data Type
+
+print(num1) # Output: 10
+print(num2) # Output: 15
+
+
+add = 5 + 3 # Addition
+print(add) # Output: 8  
+
+sub = 6 - 1 # Subtraction
+print(sub) # Output: 5
+
+exp = 3**3 # Exponentiation
+print(exp) # Output: 27
+
+mod = 10 % 3 # Modulus
+print(mod) # Output: 1
+
+floor = 5 // 3 # Floor Division
+print(floor) # Output: 1
--- index.py
+++ index.py
@@ -1,1 +1,18 @@
 print("Hello world")
+
+number = 10
+print(number)
+
+
+age = 27 # Integer Data Type
+marks = 90.5 # Float Data Type
+
+name = "Collins the Guru" # String Data Type
+print(name)
+
+
+# List Data Types
+
+languages = ["python", "HTML", "CSS", 3, 5.7, True]
+print(languages)
+print(languages[-6])
